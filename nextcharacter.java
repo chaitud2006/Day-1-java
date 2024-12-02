0bd6e39d0cb99d5a00ac15bfd2c74d1f796efc87
chaitud2006 <cc2214574@gmail.com>
2024-12-02 15:13:26 +0530

    Add files via upload

---
+++ nextcharacter.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class nextcharacter
+{
+    public static void main(String args[])
+    {
+        char c='A';
+        int b=c;
+        b+=1;
+        char v=(char)b;
+        System.out.println(v);
+
+    }
+}
